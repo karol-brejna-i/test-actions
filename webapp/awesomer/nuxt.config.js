66cf5922595883ed8ac87b8d7ff1dd7c61479386
karol-brejna-i <karolbrejna@apache.org>
2022-11-01 09:06:55 +0100

    Add webapp mock

---
+++ webapp/awesomer/nuxt.config.js
@@ -0,0 +1,72 @@
+export default {
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: false,
+
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
+
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'awesomer',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ['view-design/dist/styles/iview.css'],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: ['@/plugins/view-ui'],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+    ],
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: '/',
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+
+    // ADDED FOR GH PAGES:
+    // router: {
+    //     base: '/test-actions/'
+    // }
+
+    // # XXX NOTE: hardcoded repo name;
+    // #Try github.event.repository.name
+
+    // it mey be even better:
+    /*
+          - name: Setup Pages
+        uses: actions/configure-pages@v2
+        with:
+          # Automatically inject router.base in your Nuxt configuration file and set
+          # target to static (https://nuxtjs.org/docs/configuration-glossary/configuration-target/).
+          #
+          # You may remove this line if you want to manage the configuration yourself.
+          static_site_generator: nuxt
+          */
+}
