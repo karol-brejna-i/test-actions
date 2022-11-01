b8e146ba31056d7ed4b2d5967175ca7a165ac684
karol-brejna-i <karolbrejna@apache.org>
2022-11-01 12:17:09 +0100

    Introduce manual nuxt config changes

--- webapp/awesomer/nuxt.config.js
+++ webapp/awesomer/nuxt.config.js
@@ -1,9 +1,10 @@
 export default {
-    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-    ssr: false,
 
-    // Target: https://go.nuxtjs.dev/config-target
-    target: 'static',
+    ssr: false,
+    router: {
+        base: "/test-actions/"
+    },
+    target: "static",
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
